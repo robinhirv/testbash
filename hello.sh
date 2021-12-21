49bf6bbf7c2514779b2b631ac6b4d57803d732cc
RobinH <student@deb11rh.vykktest.lan>
2021-12-21 10:09:05 +0200

    Versioon 1.2

--- hello.sh
+++ hello.sh
@@ -3,4 +3,21 @@
 #Date: 21.12.2021
 #Version: 0.0.0.1
 
+RXED='\033[0;31m'
+NXC='\033[0m'
+
+RED='\033[0;31m'
+NC='\033[0m'
+YELLOW='\033[0;33m'
+XYELLOW='\033[0;93m'
+XGREEN='\033[0;92m'
+XRED='\033[0;91m'
+XBLUE='\033[0;94m'
+XPURPLE='\033[0;95m'
+
+printf "${XYELLOW}-----------------------------------------------------------${NC}\n"
 printf "Tere maailm!\n"
+printf "${RXED}See tekst on punane ju!${NC}\n"
+printf "Tänane kuupäev on:\n"
+date
+printf "${XYELLOW}-----------------------------------------------------------${NC}\n"
